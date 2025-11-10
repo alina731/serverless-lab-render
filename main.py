961e721f9d7aed013858e0841981e6af9259ac79
alina <alinavinogradova50@gmail.com>
2025-11-10 16:25:41 +0300

    Add /echo endpoint

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Hello, Serverless! 🚀\n", 200, {'Content-Type': 'text/plain'}
-if __name__ == '__main__':
+
+@app.route('/echo', methods=['POST'])
+def echo():
+    data = request.get_json()
+    return jsonify({
+        "status": "received",
+        "you_sent": data,
+        "length": len(str(data)) if data else 0
+    })
+
+if name == '__main__':
     app.run(host='0.0.0.0', port=5000)
